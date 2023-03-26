2614395f6eb07d636e943bb187695c319b3f5aa2
PiangGG <974805475@qq.com>
2023-03-27 01:04:12 +0800

    角色切换逻辑

--- Source/EggTowerPlanRuntime/Tool/StructLib.h
+++ Source/EggTowerPlanRuntime/Tool/StructLib.h
@@ -3,10 +3,21 @@
 #pragma once
 
 #include "CoreMinimal.h"
-
+#include "EnumLib.h"
+#include "StructLib.generated.h"
 /**
  * 
  */
+
+USTRUCT(BlueprintType)
+struct EGGTOWERPLANRUNTIME_API FPCModeChange
+{
+	GENERATED_USTRUCT_BODY()
+public:
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category= PC)
+	EControllerState PCState;
+};
+
 class EGGTOWERPLANRUNTIME_API StructLib
 {
 public:
